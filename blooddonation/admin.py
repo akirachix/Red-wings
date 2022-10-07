711265721d3b04382d0a2a9503f72a2270f6aac4
Gumato-Roba <gumatorobba@gmail.com>
2022-10-07 16:50:37 +0300

    added models

--- blooddonation/admin.py
+++ blooddonation/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Donor,Notification
+from .models import Donor,Notification, Request,Statistic
 
 # Register your models here.
 class DonorAdmin(admin.ModelAdmin):
@@ -12,3 +12,14 @@ class NotificationAdmin(admin.ModelAdmin):
     list_display=("date_created", "message","recipient","time","origin","title")
     search_fields=("date_created","title", "message",)
 admin.site.register(Notification, NotificationAdmin)
+
+
+class RequestAdmin(admin.ModelAdmin):
+    list_display=("name", "blood_type","time","date")
+    search_fields=("name", "blood_type","recipient","time","date")
+admin.site.register(Request, RequestAdmin)
+
+class StatisticAdmin(admin.ModelAdmin):
+    list_display=("year", "donors")
+    search_fields=("year", "donors")
+admin.site.register(Statistic, StatisticAdmin)
