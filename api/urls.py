233ec54803ed8133fa1880070e3a43800d17785b
lucy <lucy@gmail.com>
2022-10-20 08:54:23 +0300

    changes on login endpoint

--- api/urls.py
+++ api/urls.py
@@ -3,8 +3,8 @@ from rest_framework import routers
 from .views import RequestViewset
 from blooddonation.models import Request
 from .views import DonorViewSet
-from .views import NewUserViewSet
 from blooddonation.models import Donor
+from .views import NewUserViewSet
 from blooddonation.models import NewUser
 
 
@@ -16,11 +16,14 @@ from blooddonation.models import NewUser
 router=routers.DefaultRouter()
 router.register(r'Request',RequestViewset,basename=Request)
 router.register(r"Donor",DonorViewSet,basename=Donor)
-router.register(r'NewUser',NewUserViewSet,basename=NewUser)
+router.register(r"NewUser",NewUserViewSet,basename=NewUser)
 
 
 urlpatterns=[
     path("",include(router.urls)),
     path("",include(router.urls)),
+    path("",include(router.urls)),
+
+    
 
 ]
